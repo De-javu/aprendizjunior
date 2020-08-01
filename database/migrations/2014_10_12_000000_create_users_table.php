b0cfe75a15787a03a44bb949e71181739a40cce4
andres pardo <andrespardo5151@gmail.com>
2020-08-01 07:36:39 -0500

    Creamos la migracion de categorias

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 60);
+            $table->enum('type', ['member', 'admin'])->default('member');
             $table->rememberToken();
             $table->timestamps();
         });
